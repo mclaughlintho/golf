ce7951743cd8c077a779a8a86de0226a093f5e55
Tom <mclaughlin.tho@gmail.com>
2015-07-13 08:48:48 +0000

    added placeholder images for up/down sort btns

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -5,7 +5,7 @@ Clearance.configure do |config|
   config.cookie_expiration = lambda { |cookies| 1.year.from_now.utc }
   config.cookie_name = 'remember_token'
   config.cookie_path = '/'
-  config.routes = false
+  config.routes = true
   config.httponly = false
   config.password_strategy = Clearance::PasswordStrategies::BCrypt
   config.redirect_url = '/'
